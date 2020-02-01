4a18ef408b3c328884c0105b8cae2231cf15d80a
devmatteini <dev.matteini@gmail.com>
2020-02-01 15:17:18 +0100

    bin: change BASEDIR variable

--- bin/create_symbolic_links.sh
+++ bin/create_symbolic_links.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-BASEDIR="$(cd .. && pwd)"
+if [ -z $1 ];then
+    echo "Usage: create_symbolic_links.sh <BASEDIR>"
+    return 1
+fi
+
+BASEDIR=$1
 
 FILES=(
     "shell/.bash_aliases"
@@ -15,6 +20,8 @@ FILES=(
     "vim/.vimrc"
 )
 
+echo -e "\e[1;34m[i] Creating symlinks in $HOME/ ...\e[0m"
+
 for file in ${FILES[@]}; do
     sourceFile="$BASEDIR/$file"
     targetFile="$HOME/$(printf "%s" "$file" | sed "s/.*\/\(.*\)/\1/g")"
@@ -22,3 +29,5 @@ for file in ${FILES[@]}; do
     ln -sfn $sourceFile $targetFile
 done;
 unset file;
+
+echo -e "\e[1;32m[✓] Symlinks created succesfully.\e[0m"
