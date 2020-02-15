605c3419e58a29310e6ab81bd6a4661949f0cca3
devmatteini <dev.matteini@gmail.com>
2020-02-15 18:46:49 +0100

    install: create editors script

---
+++ install/editors.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y vim
+
+sudo snap install code --classic
+sudo snap install rider --classic
--- install/snap.sh
+++ install/snap.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-sudo snap install code --classic
-sudo snap install rider --classic
-sudo snap install sublime-text --classic
 sudo snap install postman
 sudo snap install gimp
 sudo snap install communitheme
