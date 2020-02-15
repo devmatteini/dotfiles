b51529bd52fc8d77342f3a885959233b4a500353
devmatteini <dev.matteini@gmail.com>
2020-02-15 20:16:22 +0100

    install: source profile before using nvm command

--- install/nvm.sh
+++ install/nvm.sh
@@ -4,4 +4,5 @@ export PROFILE=~/.bash_profile
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 
 # Install node latest v12.x.x
+source ~/.bash_profile
 nvm install 12
