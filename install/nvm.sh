d1b370ca068cbab40c78ba96596ca87e1705688c
devmatteini <dev.matteini@gmail.com>
2020-02-15 21:35:41 +0100

    install: source nvm instead of profile for the current shell

--- install/nvm.sh
+++ install/nvm.sh
@@ -3,6 +3,10 @@
 export PROFILE=~/.bash_profile
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 
+# Source nvm for the current shell
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 # Install node latest v12.x.x
-source ~/.bash_profile
 nvm install 12
