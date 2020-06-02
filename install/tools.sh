d3a5f189edcfdea02c225a01dae1da265ca3194b
devmatteini <dev.matteini@gmail.com>
2020-06-02 13:16:14 +0200

    install: comment some tools

--- install/tools.sh
+++ install/tools.sh
@@ -5,13 +5,13 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 # thefuck https://github.com/nvbn/thefuck
-pip3 install thefuck
+# pip3 install thefuck
 
 # tldr https://github.com/tldr-pages/tldr/
 sudo npm install -g tldr
 
 # Joplin https://github.com/laurent22/joplin
-wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash
+# wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash
 
 # AWS cli
 pip3 install awscli --upgrade --user
