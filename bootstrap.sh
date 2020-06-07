498620e5662e884b91d977c343fab62ff377ff5e
devmatteini <dev.matteini@gmail.com>
2020-06-07 19:53:03 +0200

    setup: load gnome-terminal config file, if using gnome-terminal

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,9 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Create symlinks to your $HOME directory
 source bin/create_symbolic_links.sh "$BASEDIR"
+
+# Load gnome-terminal config file, if using gnome-terminal
+terminal=$(< "/proc/${1:-$PPID}/comm")
+if [ "$terminal" == "gnome-terminal-" ]; then
+    source bin/gnome_terminal.sh gnome-terminal.dconf
+fi
