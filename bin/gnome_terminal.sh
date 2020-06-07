39cee411eaa2e7033195cfc37cc28194e20c2230
devmatteini <dev.matteini@gmail.com>
2020-06-07 19:13:17 +0200

    bin: implemented script to load gnome terminal config file

---
+++ bin/gnome_terminal.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Usage: $(basename "${BASH_SOURCE[0]}") <path/to/file.dconf>"
+    return 1
+fi
+
+if [[ ! -e "$1" ]];then
+    echo -e "\e[1;31m[X] File ${1} does not exist\e[0m"
+    return 1
+fi
+
+echo -e "\e[1;34m[i] Loading ${1} in /org/gnome/terminal/legacy/profiles:/\e[0m"
+
+if dconf load /org/gnome/terminal/legacy/profiles:/ < "$1"; then
+    echo -e "\e[1;32m[✓] Loaded ${1} succesfully.\e[0m"
+else
+    echo -e "\e[1;31m[X] Cannot load ${1} in /org/gnome/terminal/legacy/profiles:/\e[0m"
+fi
