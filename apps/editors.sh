f48ef3bc6a0a8e61a74fc1dede0be976d6055046
devmatteini <dev.matteini@gmail.com>
2021-10-31 19:45:33 +0100

    apps: created python installer to add more flexibility on which programs you need to install
    By default all packages will be installed.
    
    To only install certain packages use:
    $ ./apps/editors --filter vim,vscode
    
    If you want to check out which packages are present:
    $ ./apps/editors --list

--- apps/editors.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt update
-sudo apt install -y vim
-
-sudo snap install code --classic
-sudo snap install rider --classic
---
+++ apps/installer.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import argparse
+from typing import Callable, List
+import os
+
+BOLD='\033[1m'
+RESET='\033[0m'
+
+class Package:
+    name:str
+    install_command:str
+
+    def __init__(self, name:str, install_command:str) -> None:
+        self.name = name
+        self.install_command = install_command
+
+def run_installer(packages: List[Package], parser: argparse.ArgumentParser) -> None:
+    args = parser.parse_args()
+
+    if args.list:
+        for package in packages:
+            print(package.name)
+        return
+
+    filters= args.filter.split(",") if args.filter else []
+    to_intall= filter(lambda x: x.name in filters, packages) if filters else packages
+    for package in to_intall:
+        print(f'Installing {BOLD}{package.name}{RESET}...')
+        os.system(package.install_command)
+
+def create_installer(name:str, packages: List[Package]) -> Callable[[], None]:
+    parser = argparse.ArgumentParser(description=f'Application installer for {BOLD}{name}{RESET}')
+    parser.add_argument('--list', action="store_true", help='List all packages')
+    parser.add_argument('--filter', type=str, help='Comma separated list of packages to install')
+
+    return lambda: run_installer(packages, parser)
