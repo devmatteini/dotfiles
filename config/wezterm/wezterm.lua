58a66dbf26a4a67e71f983148d3ff8d2a508cbf1
Cosimo Matteini <dev.matteini@gmail.com>
2023-02-26 23:24:27 +0100

    config - wezterm: use Gruvbox as theme and decrease inactive pane brightness

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require "wezterm"
 return {
   font = wezterm.font "FiraCode Nerd Font",
   font_size = 15.0,
-  color_scheme = "Snazzy",
+  color_scheme = "Gruvbox dark, hard (base16)",
 
   scrollback_lines = 10000,
 
@@ -12,5 +12,9 @@ return {
     right = 2,
     top = 2,
     bottom = 2,
+  },
+
+  inactive_pane_hsb = {
+    brightness = 0.5,
   },
 }
