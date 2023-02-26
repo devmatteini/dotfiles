e84195a252726b45d061db7a9e39ab936732a055
Cosimo Matteini <dev.matteini@gmail.com>
2023-02-27 00:19:22 +0100

    config - wezterm: improve hyperlinks rules

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -17,4 +17,25 @@ return {
   inactive_pane_hsb = {
     brightness = 0.5,
   },
+  hyperlink_rules = {
+    -- Linkify things that look like URLs and the host has a TLD name.
+    -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+    {
+      regex = '\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b',
+      format = '$0',
+    },
+    -- Linkify things that look like URLs with numeric addresses as hosts.
+    -- E.g. http://127.0.0.1:8000 for a local development server,
+    -- or http://192.168.1.1 for the web interface of many routers.
+    {
+			regex = "\\b\\w+://(?:[\\w.-]+)(:\\d+)?\\S*\\b",
+			format = "$0",
+		},
+    -- file:// URI
+    -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+    {
+      regex = [[\bfile://\S*\b]],
+      format = '$0',
+    },
+  },
 }
