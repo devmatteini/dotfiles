778a2dfe350c71876d7fe7d57543c788b65c8d86
Cosimo Matteini <dev.matteini@gmail.com>
2023-03-19 23:04:41 +0100

    config - wezterm: format file

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -4,13 +4,10 @@ local act = wezterm.action
 return {
   -- Avoid spawning login shell that causes starship to always shop the `jobs` module
   default_prog = { os.getenv("SHELL") },
-
   font = wezterm.font "FiraCode Nerd Font",
   font_size = 15.0,
   color_scheme = "Gruvbox dark, hard (base16)",
-
   scrollback_lines = 10000,
-
   window_padding = {
     left = 2,
     right = 2,
@@ -32,9 +29,9 @@ return {
     -- E.g. http://127.0.0.1:8000 for a local development server,
     -- or http://192.168.1.1 for the web interface of many routers.
     {
-			regex = "\\b\\w+://(?:[\\w.-]+)(:\\d+)?\\S*\\b",
-			format = "$0",
-		},
+      regex = "\\b\\w+://(?:[\\w.-]+)(:\\d+)?\\S*\\b",
+      format = "$0",
+    },
     -- file:// URI
     -- Compiled-in default. Used if you don't specify any hyperlink_rules.
     {
@@ -42,9 +39,8 @@ return {
       format = '$0',
     },
   },
-
   keys = {
-    { key = 'LeftArrow', mods = 'SHIFT|ALT', action = act.MoveTabRelative(-1) },
+    { key = 'LeftArrow',  mods = 'SHIFT|ALT', action = act.MoveTabRelative(-1) },
     { key = 'RightArrow', mods = 'SHIFT|ALT', action = act.MoveTabRelative(1) },
   },
 }
