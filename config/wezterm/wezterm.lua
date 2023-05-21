3c8dca1ff0f2c9c9752f3792385df9cafdeabda9
Cosimo Matteini <dev.matteini@gmail.com>
2023-05-21 22:38:07 +0200

    wezterm: use new config_builder api

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,6 +2,11 @@ local wezterm = require "wezterm"
 local project = require "project"
 local act = wezterm.action
 
+local config = {}
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
 wezterm.on('gui-startup', function()
   project.startup("WZ_PROJECT", "projects", wezterm)
 end)
@@ -19,28 +24,32 @@ if wezterm.gui then
   end
 end
 
-return {
-  -- Avoid spawning login shell that causes starship to always shop the `jobs` module
-  default_prog = { os.getenv("SHELL") },
-  font = wezterm.font "FiraCode Nerd Font",
-  font_size = 15.0,
-  color_scheme = "Snazzy",
-  force_reverse_video_cursor = true,
-  scrollback_lines = 10000,
-  window_padding = {
-    left = 2,
-    right = 2,
-    top = 2,
-    bottom = 2,
-  },
-  inactive_pane_hsb = {
-    brightness = 0.5,
-  },
-  keys = {
-    { key = 'LeftArrow',  mods = 'SHIFT|ALT', action = act.MoveTabRelative(-1) },
-    { key = 'RightArrow', mods = 'SHIFT|ALT', action = act.MoveTabRelative(1) },
-  },
-  key_tables = {
-    copy_mode = copy_mode
-  }
+-- Avoid spawning login shell that causes starship to always shop the `jobs` module
+config.default_prog = { os.getenv("SHELL") }
+
+config.font = wezterm.font "FiraCode Nerd Font"
+config.font_size = 15.0
+
+config.color_scheme = "Snazzy"
+config.force_reverse_video_cursor = true
+
+config.scrollback_lines = 10000
+config.window_padding = {
+  left = 2,
+  right = 2,
+  top = 2,
+  bottom = 2,
 }
+config.inactive_pane_hsb = {
+  brightness = 0.5,
+}
+
+config.keys = {
+  { key = 'LeftArrow',  mods = 'SHIFT|ALT', action = act.MoveTabRelative(-1) },
+  { key = 'RightArrow', mods = 'SHIFT|ALT', action = act.MoveTabRelative(1) },
+}
+config.key_tables = {
+  copy_mode = copy_mode
+}
+
+return config
