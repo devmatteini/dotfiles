e6254432d16869a39b40275150b99cdb5a29547d
Cosimo Matteini <dev.matteini@gmail.com>
2023-12-29 16:56:52 +0100

    apps - fnm: add yarn bash completions

--- apps/fnm.sh
+++ apps/fnm.sh
@@ -3,6 +3,7 @@
 # GitHub: https://github.com/Schniz/fnm
 
 INSTALL_DIR="$HOME/.local/apps/fnm"
+BASH_COMPLETIONS="$HOME/.local/share/bash-completion/completions"
 
 curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "$INSTALL_DIR" --skip-shell
 
@@ -12,9 +13,12 @@ ln -sf "$INSTALL_DIR/fnm" "$HOME/.local/bin/fnm"
 fnm install --corepack-enabled 18
 fnm install --corepack-enabled 20
 
-fnm completions --shell bash > "$HOME"/.local/share/bash-completion/completions/fnm
+fnm completions --shell bash > "$BASH_COMPLETIONS/fnm"
 
 # *************** NPM/YARN ***************
 
-# Setup npm
-npm completion > "$HOME"/.local/share/bash-completion/completions/npm
+# npm bash completions
+npm completion > "$BASH_COMPLETIONS/npm"
+
+# yarn bash completions
+curl -o "$HOME/yarn" https://raw.githubusercontent.com/dsifford/yarn-completion/master/yarn-completion.bash
