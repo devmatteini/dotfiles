65b02df3f274dfc27dd12f30b284480f389092ab
Cosimo Matteini <dev.matteini@gmail.com>
2024-02-09 15:14:40 +0100

    bootstrap: add instructions to change default shell

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+white="\e[1;37m"
+reset="\e[0m"
+green="\e[1;32m"
+underline="\e[4m"
+
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 "$BASEDIR"/bootstrap/file_system.sh
 "$BASEDIR"/bootstrap/create_symbolic_links.sh "$BASEDIR"
 "$BASEDIR"/bootstrap/dependencies.sh
+
+echo -e "${green}Bootstrap completed!$reset\n"
+echo -e "If you want to set ${underline}zsh${reset} as default shell run:\n${white}chsh -s \$(which zsh)${reset}"
