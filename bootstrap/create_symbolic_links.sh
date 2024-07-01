1848cff91704a7d2b22d7bea7c4a70c8a695906c
Cosimo Matteini <dev.matteini@gmail.com>
2024-07-02 00:12:10 +0200

    move all configuration files to configs/ directory

--- bootstrap/create_symbolic_links.sh
+++ bootstrap/create_symbolic_links.sh
@@ -6,6 +6,7 @@ if [ -z "$1" ];then
 fi
 
 BASEDIR=$1
+CONFIGS="configs"
 
 # $1 = source_file, $2 = target_file
 create_symlink() {
@@ -17,6 +18,21 @@ create_symlink() {
     else
         echo "$source_file -> $target_file"
     fi
+}
+
+# $1 = relative_source_file, $2 = target_directory
+symlink_file() {
+    file=$1
+    target_directory=$2
+
+    echo -e "\e[1;34m[i] Creating symlinks for '$file' in '$target_directory'\e[0m"
+
+    filename=$(basename "$file")
+
+    source_file="$BASEDIR/$file"
+    target_file="$target_directory/$filename"
+
+    create_symlink "$source_file" "$target_file"
 }
 
 # $1 = source_directory, $2 = target_directory
@@ -40,28 +56,23 @@ symlink_dir_files() {
 }
 
 # $1 = source_directory, $2 = target_directory
-symlink_dirs() {
+symlink_dir() {
     local directory=$1
     local target_directory=$2
 
-    # NOTE: -mindepth 1 is to avoid including $directory itself
-    readarray -t DIRS < <(find "$directory" -maxdepth 1 -mindepth 1 -type d)
+    dir_name=$(basename "$directory")
+    target_dir="$target_directory/$dir_name"
 
-    for source_dir in "${DIRS[@]}"; do
-        dir_name=$(basename "$source_dir")
-        target_dir="$target_directory/$dir_name"
-
-        mkdir -p "$target_dir"
-        symlink_dir_files "$source_dir" "$target_dir"
-    done;
+    mkdir -p "$target_dir"
+    symlink_dir_files "$directory" "$target_dir"
 }
 
-symlink_dir_files "bash" "$HOME"
-symlink_dir_files "zsh" "$HOME"
-symlink_dir_files "git" "$HOME"
-symlink_dir_files "vim" "$HOME"
-
-symlink_dir_files "config" "$HOME/.config"
-symlink_dirs "config" "$HOME/.config"
+symlink_dir_files "$CONFIGS/bash" "$HOME"
+symlink_dir_files "$CONFIGS/zsh" "$HOME"
+symlink_file "$CONFIGS/.gitconfig" "$HOME"
+symlink_file "$CONFIGS/.vimrc" "$HOME"
+symlink_file "$CONFIGS/starship.toml" "$HOME/.config"
+symlink_dir "$CONFIGS/wezterm" "$HOME/.config"
+symlink_dir "$CONFIGS/atuin" "$HOME/.config"
 
 echo -e "\e[1;32m[✓] Symlinks created succesfully.\e[0m"
