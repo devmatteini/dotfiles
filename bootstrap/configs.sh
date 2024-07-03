63d1799bc13237980371461b75603c93609f2f20
Cosimo Matteini <dev.matteini@gmail.com>
2024-07-03 22:08:07 +0200

    bootstrap - configs: provide absolute paths to symlink_* functions

--- bootstrap/configs.sh
+++ bootstrap/configs.sh
@@ -6,7 +6,7 @@ if [ -z "$1" ];then
 fi
 
 BASEDIR=$1
-CONFIGS="configs"
+CONFIGS="$BASEDIR/configs"
 
 # $1 = source_file, $2 = target_file
 create_symlink() {
@@ -29,7 +29,7 @@ symlink_file() {
 
     filename=$(basename "$file")
 
-    source_file="$BASEDIR/$file"
+    source_file="$file"
     target_file="$target_directory/$filename"
 
     create_symlink "$source_file" "$target_file"
@@ -39,11 +39,10 @@ symlink_file() {
 symlink_dir_files() {
     local directory=$1
     local target_directory=$2
-    local full_path="$BASEDIR/$directory"
 
     echo -e "\e[1;34m[i] Creating symlinks for '$directory' in '$target_directory'\e[0m"
 
-    readarray -t DIR_FILES < <(find "$full_path" -maxdepth 1 -type f)
+    readarray -t DIR_FILES < <(find "$directory" -maxdepth 1 -type f)
     
     for file in "${DIR_FILES[@]}"; do
         filename=$(basename "$file")
