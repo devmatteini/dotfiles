a1dfecd042c4331d377cb4c8b789f821ab77e5de
Cosimo Matteini <dev.matteini@gmail.com>
2024-08-20 02:29:21 +0200

    add mpv simple config

--- bootstrap/configs.sh
+++ bootstrap/configs.sh
@@ -74,5 +74,6 @@ symlink_file "$CONFIGS/.vimrc" "$HOME"
 symlink_file "$CONFIGS/starship.toml" "$HOME/.config"
 symlink_dir "$CONFIGS/wezterm" "$HOME/.config"
 symlink_dir "$CONFIGS/atuin" "$HOME/.config"
+symlink_file "$CONFIGS/mpv.conf" "$HOME/.config/mpv"
 
 echo -e "\e[1;32m[✓] Symlinks created succesfully.\e[0m"
